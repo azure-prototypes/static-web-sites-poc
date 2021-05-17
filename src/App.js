dd5fd6a03941754c26eaf005fd2c6d18142a9e33
Serhii <Soboliev>
2021-05-17 20:56:19 +0300

    15

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import PersonList from './PersonList';
-import PriceListList from './PriceListList';
 
 
 function App() {
-  return <div><PersonList/> <PriceListList/></div>;
+  return <div><PersonList/></div>;
 }
 
 export default App;
