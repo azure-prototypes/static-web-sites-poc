5e8969159c187df310f6bbf6927489cfe30bcf75
Serhii <Soboliev>
2021-05-17 20:38:59 +0300

    11

--- src/PriceListList.js
+++ src/PriceListList.js
@@ -7,17 +7,17 @@ export default class PriceListList extends React.Component {
     persons: []
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     await axios.get(`http://20.49.139.146/pricing/api/v1/price-lists?countryId=&page=1&size=50&name=&type=&sort=active,DESC`,{
         auth: {
             username: 'Serhii.Soboliev',
             password: 'fypN2F5NFfE94UuyPnrTmZ'
           }
     })
-      .then(res => {
+    .then(res => {
         const priceListList = res.data;
         this.setState({ priceListList });
-      })
+    })
   }
 
   render() {
